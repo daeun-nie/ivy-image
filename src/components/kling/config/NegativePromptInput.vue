f030035dc80eed64b7fda38ddb87b01a8a827fb3
daeun-nie <050610220853@st.buh.edu.vn>
2025-05-29 21:57:53 +0700

    Update NegativePromptInput.vue

--- src/components/kling/config/NegativePromptInput.vue
+++ src/components/kling/config/NegativePromptInput.vue
@@ -10,7 +10,7 @@
       type="textarea"
       class="prompt"
       :placeholder="$t('kling.placeholder.negativePrompt')"
-      :maxlength="200"
+      :maxlength="20000"
       show-word-limit
     />
   </div>

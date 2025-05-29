4cfd207743d0ce6d2c009877f76007a7d6df5b68
daeun-nie <050610220853@st.buh.edu.vn>
2025-05-29 21:55:36 +0700

    Update PromptInput.vue

--- src/components/luma/config/PromptInput.vue
+++ src/components/luma/config/PromptInput.vue
@@ -10,7 +10,7 @@
       type="textarea"
       class="prompt"
       :placeholder="$t('luma.placeholder.prompt')"
-      :maxlength="2000"
+      :maxlength="20000"
       show-word-limit
     />
   </div>

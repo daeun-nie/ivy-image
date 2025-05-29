7dea6c820a7958a46103313ab5dca399fd7e7433
daeun-nie <050610220853@st.buh.edu.vn>
2025-05-29 21:55:22 +0700

    Update PromptInput.vue

--- src/components/flux/config/PromptInput.vue
+++ src/components/flux/config/PromptInput.vue
@@ -10,7 +10,7 @@
       type="textarea"
       class="prompt"
       :placeholder="$t('flux.placeholder.prompt')"
-      :maxlength="300"
+      :maxlength="3000"
       show-word-limit
     />
   </div>
